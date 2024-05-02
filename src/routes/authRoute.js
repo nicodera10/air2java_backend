08aef28a1ec8de5dd27a376e9d28a0284bf9be8a
nicodera10 <niiicolio@hotmail.fr>
2024-05-02 14:53:03 +0200

    fonction de connexion ok, ajout d'un token, work in progress

---
+++ src/routes/authRoute.js
@@ -0,0 +1,36 @@
+// src/routes/authRoute.js
+const express = require("express");
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const { AppUser } = require('../models/appuser');
+const jwt = require('jsonwebtoken');
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    // Recherchez l'utilisateur par nom d'utilisateur
+    const user = await AppUser.findOne({ where: { name_appuser: username } });
+
+    if (!user) {
+      throw new Error('Nom d\'utilisateur  incorrect');
+    }
+
+    // Vérifiez si le mot de passe est correct en le comparant avec le hash stocké
+    const passwordMatch = await bcrypt.compare(password, user.password_appuser);
+
+    if (!passwordMatch) {
+      throw new Error('mot de passe incorrect');
+    }
+
+    // Générez le token JWT
+    const token = jwt.sign({ userId: user.id_appuser }, { userType: user.type_appuser}, process.env.RANDOM_KEYS, { expiresIn: '1h' });
+
+    // Si les identifiants sont corrects, renvoyez les données de l'utilisateur
+    res.json({ token });
+  } catch (error) {
+    console.error('Erreur de connexion :', error);
+    res.status(401).json({ error: error.message });
+  }
+});
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -17,24 +17,42 @@ const bandRouter = require(bandRoutesPath);
 const personRoutesPath = process.env.PERSON_ROUTES;
 const personRouter = require(personRoutesPath);
 
+// Import des routes d'authentification
+const authRoutes = require('./routes/authRoute');
+
 // Configurez ici vos middlewares
 
+// Middleware pour parser les requêtes JSON
+app.use(express.json());
+
+// Middleware pour les CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // Gérer les pré-vérifications CORS (OPTIONS)
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
+
 
 /**
  * Routers
  */
+
+// Utilisation des routes d'authentification
+app.use('/auth', authRoutes);
+
 app.use('/appuser', appuserRouter);
 app.use('/festival', festivalRouter);
 app.use('/band', bandRouter);
 app.use('/person', personRouter);
 
-
+// Lancement du serveur
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
