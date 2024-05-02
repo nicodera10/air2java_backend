e792c8a2fcea838e6419d88c5f40c6fce342598d
nicodera10 <niiicolio@hotmail.fr>
2024-05-02 09:42:46 +0200

    ajout middlware CORS

--- src/server.js
+++ src/server.js
@@ -19,6 +19,12 @@ const personRouter = require(personRoutesPath);
 
 // Configurez ici vos middlewares
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 /**
  * Routers
