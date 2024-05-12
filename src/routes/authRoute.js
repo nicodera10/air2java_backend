769dffb94e101e465c8958c9c7839aae82d228dd
nicodera10 <niiicolio@hotmail.fr>
2024-05-12 12:23:16 +0200

    modification de l'authentification pour passer le token de manière sécurisé et via cookie en https

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -12,22 +12,26 @@ router.post('/login', async (req, res) => {
     const user = await AppUser.findOne({ where: { name_appuser: username } });
 
     if (!user) {
-      throw new Error('Nom d\'utilisateur  incorrect');
+      throw new Error('Nom d\'utilisateur incorrect');
     }
 
     // Vérifiez si le mot de passe est correct en le comparant avec le hash stocké
     const passwordMatch = await bcrypt.compare(password, user.password_appuser);
 
     if (!passwordMatch) {
-      throw new Error('mot de passe incorrect');
+      throw new Error('Mot de passe incorrect');
     }
 
     // Générez le token JWT
-    const token = jwt.sign({ userId: user.id_appuser, userType: user.type_appuser }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const tokenPayload = { userId: user.id_appuser, userType: user.type_appuser };
+    const token = jwt.sign(tokenPayload, process.env.JWT_SECRET, { expiresIn: '1h' });
 
+    // Renvoyer le token JWT dans un cookie sécurisé (httpsOnly)
+    res.cookie('token', token, { httpOnly: true, secure: true });
 
-    // Si les identifiants sont corrects, renvoyez les données de l'utilisateur
-    res.json({ token });
+    // Renvoyer le nom d'utilisateur dans le corps de la réponse
+    res.json({ userName: user.name_appuser });
+
   } catch (error) {
     console.error('Erreur de connexion :', error);
     res.status(401).json({ error: error.message });
