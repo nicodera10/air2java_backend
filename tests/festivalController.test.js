deb844a025fde29d0121760a68f0fb3c0bf5d3e3
nicodera10 <niiicolio@hotmail.fr>
2024-05-15 15:52:51 +0200

    rajout du test pour la route getOneFestival

--- tests/festivalController.test.js
+++ tests/festivalController.test.js
@@ -16,3 +16,30 @@ describe('GET /festival/latest', () => {
     expect(response.body).toBeTruthy();
   });
 });
+
+describe('GET /festival/:id', () => {
+    it('responds with JSON containing the details of a specific festival', async () => {
+      // Supposons que vous ayez un ID de festival valide dans votre base de données
+      const festivalId = 1;
+  
+      const response = await request(app).get(`/festival/${festivalId}`);
+      
+      // Vérifiez le statut de la réponse
+      expect(response.statusCode).toBe(200);
+      
+      // Vérifiez si la réponse contient les détails du festival
+      expect(response.body).toBeTruthy();
+      expect(response.body.id_fest).toBe(festivalId); // Assurez-vous que les détails correspondent à l'ID du festival demandé
+      // Vous pouvez également ajouter d'autres attentes pour les détails du festival en fonction de votre implémentation
+    });
+  
+    it('responds with 404 if the festival does not exist', async () => {
+      // Supposons que vous ayez un ID de festival qui n'existe pas dans votre base de données
+      const invalidFestivalId = 999;
+  
+      const response = await request(app).get(`/festival/${invalidFestivalId}`);
+      
+      // Vérifiez si la réponse est une erreur 404 (Not Found)
+      expect(response.statusCode).toBe(404);
+    });
+  });
